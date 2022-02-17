7f42328724da1cc18a35178bdb562a9ef4316018
Justin Mulroney <your@email.comjustinmulroney@gmail.com>
2022-02-17 15:28:43 -0500

    adding test cases and refractored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,19 @@
 const request = require('request');
-let catType = process.argv.splice(2);
 
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      callback(error,null);
+    }
+    const [data] = JSON.parse(body);
+  
+    if (!data) {
+      callback(null,'Invalid Breed');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + catType, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
-  const [data] = JSON.parse(body);
+    } else {
+      callback(null,data.description);
+    }
+  });
+};
 
-  if (!data) {
-    console.log('no breed given');
-  } else {
-    console.log(data.description);
-  }
-});
-
+module.exports = { fetchBreedDescription };
